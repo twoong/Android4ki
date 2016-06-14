202a2f4f5a3efa3b1f0c7102d5ea5a20bdeba367
Tae Woong <twoong.yoon@gmail.com>
2016-06-14 11:59:52 +0900

    Database update

--- app/src/main/java/com/twoong/android4ki/database/DbActivity.java
+++ app/src/main/java/com/twoong/android4ki/database/DbActivity.java
@@ -17,7 +17,6 @@ public class DbActivity extends AppCompatActivity {
     private EditText mTitle;
     private EditText mSubtitle;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,21 +31,46 @@ public class DbActivity extends AppCompatActivity {
         findViewById(R.id.insert_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //삽입
+            //삽입
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID, mEntryId.getText().toString());
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, mTitle.getText().toString());
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, mSubtitle.getText().toString());
+
+            long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME,
+                    null,
+                    values);
+            if (newRowId != -1) {
+                Toast.makeText(DbActivity.this, "정상 삽입", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(DbActivity.this, "삽입 에러", Toast.LENGTH_SHORT).show();
+            }
+            }
+        });
+
+        findViewById(R.id.update_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //업데이트
                 SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
                 ContentValues values = new ContentValues();
-                values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID, mEntryId.getText().toString());
                 values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, mTitle.getText().toString());
                 values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, mSubtitle.getText().toString());
 
-                long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME,
-                        null,
-                        values);
-                if(newRowId != -1){
-                    Toast.makeText(DbActivity.this, "정상 삽입", Toast.LENGTH_SHORT).show();
+                String where = FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID + "=?";
+
+                int updatedRow = db.update(FeedReaderContract.FeedEntry.TABLE_NAME,
+                        values,
+                        where,
+                        new String[]{mEntryId.getText().toString()});
+
+                if(updatedRow != 0){
+                    Toast.makeText(DbActivity.this, "수정 되었습니다", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(DbActivity.this, "삽입 에러", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(DbActivity.this, "수정 에러", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/twoong/android4ki/database/FeedReaderContract.java
+++ app/src/main/java/com/twoong/android4ki/database/FeedReaderContract.java
@@ -8,12 +8,6 @@ import android.provider.BaseColumns;
  */
 public final class FeedReaderContract {
 
-
-
-
-
-
-
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
     public FeedReaderContract() {}
