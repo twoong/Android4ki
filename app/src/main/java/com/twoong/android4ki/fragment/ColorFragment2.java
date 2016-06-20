92dc26f8fbd9ed3fcb6b2b0abadd6c83f5898dbe
taewoong <twoog.yoon@gmail.com>
2016-06-20 11:34:22 +0900

    Interface로 Listner 구현

--- app/src/main/java/com/twoong/android4ki/fragment/ColorFragment2.java
+++ app/src/main/java/com/twoong/android4ki/fragment/ColorFragment2.java
@@ -1,6 +1,5 @@
 package com.twoong.android4ki.fragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,6 +14,8 @@ import com.twoong.android4ki.R;
  */
 public class ColorFragment2 extends Fragment implements View.OnClickListener {
 
+    private OnClickMyListner mListner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,18 +26,43 @@ public class ColorFragment2 extends Fragment implements View.OnClickListener {
         return view;
     }
 
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnClickMyListner){
+//            mListner = (OnClickMyListner) context;
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListner = null;
+//    }
+
+    public void setOnMyClickListner(OnClickMyListner listner) {
+        mListner = listner;
+    }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.red_button:
-                getView().setBackgroundColor(Color.RED);
-                break;
-            case R.id.green_button:
-                getView().setBackgroundColor(Color.GREEN);
-                break;
-            case R.id.blue_button:
-                getView().setBackgroundColor(Color.BLUE);
-                break;
+//        switch (v.getId()) {
+//            case R.id.red_button:
+//                getView().setBackgroundColor(Color.RED);
+//                break;
+//            case R.id.green_button:
+//                getView().setBackgroundColor(Color.GREEN);
+//                break;
+//            case R.id.blue_button:
+//                getView().setBackgroundColor(Color.BLUE);
+//                break;
+//        }
+        if(mListner != null){
+            mListner.onClick(v, getView());
         }
+    }
+
+    public interface OnClickMyListner {
+        void onClick(View clickedView, View colorChangeView);
     }
 }
--- app/src/main/java/com/twoong/android4ki/fragment/FragmentExamActivity.java
+++ app/src/main/java/com/twoong/android4ki/fragment/FragmentExamActivity.java
@@ -1,7 +1,9 @@
 package com.twoong.android4ki.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.twoong.android4ki.R;
 
@@ -12,6 +14,23 @@ public class FragmentExamActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_exam);
 
+        ColorFragment2 colorFragment2 = (ColorFragment2) getSupportFragmentManager().findFragmentById(R.id.color_frag);
 
+        colorFragment2.setOnMyClickListner(new ColorFragment2.OnClickMyListner() {
+            @Override
+            public void onClick(View clickedView, View colorChangeView) {
+                switch (clickedView.getId()) {
+                    case R.id.red_button:
+                        colorChangeView.setBackgroundColor(Color.RED);
+                        break;
+                    case R.id.green_button:
+                        colorChangeView.setBackgroundColor(Color.GREEN);
+                        break;
+                    case R.id.blue_button:
+                        colorChangeView.setBackgroundColor(Color.BLUE);
+                        break;
+                }
+            }
+        });
     }
 }
